9c9df0613dbc6da85752636fff8fd5269d45d275
adityapaul26 <adityapaul2603@gmail.com>
2026-09-07 17:19:56 +0530

    ...

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth.routes");
 const cors = require("cors");
+const interviewRouter = require("./routes/interview.routes");
 
 const app = express();
 
+// middlewares
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -13,6 +15,9 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
+
+// routes
+app.use("/api/interview", interviewRouter);
 app.use("/api/auth", authRouter);
 
 module.exports = app;
