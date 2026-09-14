414380cfe48ea4bf7a66eba73d6e78812642dd5d
adityapaul26 <adityapaul2603@gmail.com>
2026-09-15 00:52:52 +0530

    idk

--- Frontend/src/features/interview/hooks/useInterview.js
+++ Frontend/src/features/interview/hooks/useInterview.js
@@ -12,4 +12,57 @@ export const useInterview = () => {
   }
   const { loading, setLoading, report, setReport, reports, setReports } =
     context;
+
+  const generateReport = async ({
+    jobDescription,
+    selfDescription,
+    resumeFile,
+  }) => {
+    setLoading(true);
+    try {
+      const data = await generateInterviewReport({
+        jobDescription,
+        selfDescription,
+        resumeFile,
+      });
+      setReport(data.interviewReport);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getReportById = async (interviewId) => {
+    setLoading(true);
+    try {
+      const report = await getAllInterviewReports(interviewId);
+      setReport(report.interviewReport);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getReports = async () => {
+    setLoading(true);
+    try {
+      const reports = await getAllInterviewReports();
+      setReports(reports);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    loading,
+    report,
+    reports,
+    generateReport,
+    getReportById,
+    getReports,
+  };
 };
